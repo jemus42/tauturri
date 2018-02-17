89764182056495c1f4d5e0ce81ff76887388a8c7
jemus42 <jemus42@quantenbrot.de>
2018-02-17 19:26:21 +0100

    More tests

--- tests/testthat/test-get_plays_by.R
+++ tests/testthat/test-get_plays_by.R
@@ -5,4 +5,14 @@ test_that("get_plays_by_date", {
   expect_is(res, "tbl")
   expect_length(res, 4)
   expect_named(res, c("date", "TV", "Movies", "Music"))
+  expect_error(get_plays_by_date(url = "", apikey = ""))
+
+})
+
+test_that("get_plays_by_top_10_users works", {
+  res <- get_plays_by_top_10_users()
+  expect_is(res, "tbl")
+  expect_length(res, 4)
+  expect_named(res, c("user", "TV", "Movies", "Music"))
+  expect_error(get_plays_by_top_10_users(url = "", apikey = ""))
 })
